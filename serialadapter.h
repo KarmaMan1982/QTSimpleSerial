d8bc9db68cf49923a1815846e390c19240e2f4f5
KarmaMan1982 <markuseberherr@googlemail.com>
2018-05-17 18:15:12 +0200

    Update serialadapter.h

--- serialadapter.h
+++ serialadapter.h
@@ -1,6 +1,14 @@
 #ifndef SERIALADAPTER_H
 #define SERIALADAPTER_H
 #include <QtSerialPort/QSerialPort>
+#include "jsonparser.h"
+
+#define CTRL_SOH	0x01
+#define CTRL_STX	0x02
+#define CTRL_ETX	0x03
+#define CTRL_EOT	0x04
+/* UART jeweils RX und TX Byte Buffer Größe */
+#define UART_RXTX_BUFFSIZE		512
 
 class SerialAdapter : public QSerialPort
 {
@@ -12,6 +20,8 @@ public:
     void sendData(QByteArray);
 private:
     bool setWriteable;
+    QByteArray inputPackage;
+    JSONParser jsonParser;
 public slots:
     void readData();
 };
